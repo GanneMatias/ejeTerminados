ac35eee9d90bbc8115c5c94491f8c36b840c142b
GanneMatias <gannematias10@gmail.com>
2022-06-21 12:18:38 -0300

    ejercicio de billetera

---
+++ Clase13ejercicio/Clase13ejercicio/Modelo/Billetera.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Clase13ejercicio.Modelo
+{
+    internal class Billetera
+    {
+        public int BilletesDe10 { get; set; }
+        public int BilletesDe20 { get; set; }
+        public int BilletesDe50 { get; set; }
+        public int BilletesDe100 { get; set; }
+        public int BilletesDe200 { get; set; }
+        public int BilletesDe500 { get; set; }
+        public int BilletesDe1000 { get; set; }
+
+
+        public decimal Total()
+        {
+            decimal total=0;
+            total = ((BilletesDe10 * 10) + (BilletesDe20 * 20) + (BilletesDe50 * 50) + (BilletesDe100 * 100) + (BilletesDe200 * 200) + (BilletesDe500 * 500) + (BilletesDe1000 * 1000));
+            return total;
+        }
+
+        public void MostrarBilletes()
+        {
+            Console.WriteLine("BILLETES:\nmonto:cantidad");
+            Console.WriteLine($"10: {BilletesDe10}\n20: {BilletesDe20}\n50: {BilletesDe50}\n100: {BilletesDe100}\n200: {BilletesDe200}\n500: {BilletesDe500}\n1000: {BilletesDe1000}");
+        }
+
+        public void Vaciar()
+        {
+            BilletesDe10 = 0;
+            BilletesDe20 = 0;
+            BilletesDe50 = 0;
+            BilletesDe100 = 0;
+            BilletesDe200 = 0;
+            BilletesDe500 = 0;
+            BilletesDe1000 = 0;
+        }
+
+        public Billetera Combinar(Billetera combinar)
+        {
+            Billetera combinada = new Billetera();
+
+            combinada.BilletesDe10 = BilletesDe10 + combinar.BilletesDe10;
+            combinada.BilletesDe20 = BilletesDe20 + combinar.BilletesDe20;
+            combinada.BilletesDe50 = BilletesDe50 + combinar.BilletesDe50;
+            combinada.BilletesDe100 = BilletesDe100 + combinar.BilletesDe100;
+            combinada.BilletesDe200 = BilletesDe200 + combinar.BilletesDe200;
+            combinada.BilletesDe500 = BilletesDe500 + combinar.BilletesDe500;
+            combinada.BilletesDe1000 = BilletesDe1000 + combinar.BilletesDe1000;
+            this.Vaciar();
+            combinar.Vaciar();
+            return combinada;
+        }
+    }
+}
+    
+
---
+++ Clase13ejercicio/Clase13ejercicio/Program.cs
@@ -0,0 +1,61 @@
+﻿/*
+Crear una clase Billetera que tenga las siguientes propiedades públicas del tipo entero:
+
+BilletesDe10
+BilletesDe20
+BilletesDe50
+BilletesDe100
+BilletesDe200
+BilletesDe500
+BilletesDe1000
+
+Agregar un método que se llame Total del tipo decimal, y que devuelva el Importe Total en la billetera teniendo en cuenta la cantidad de billetes que se tenga de cada tipo.
+
+Agregar un metodo que se llame Combinar, que reciba como parámetro otra billetera y que devuelva una nueva Billetera con la suma o combinacion del dinero de ambas billeteras.  
+Una vez combinadas las 2 billeteras (y creada la tercera), las 2 primeras billeteras deberan quedar en cero. (Sin billetes)
+*/
+
+using Clase13ejercicio.Modelo;
+
+decimal total=0;
+
+Billetera B1 = new Billetera();
+Billetera B2 = new Billetera();
+
+B1.BilletesDe10 = 1;
+B1.BilletesDe20 = 2;
+B1.BilletesDe50 = 3;
+B1.BilletesDe100 = 4;
+B1.BilletesDe200 = 5;
+B1.BilletesDe500 = 6;
+B1.BilletesDe1000 = 7;
+
+total = B1.Total();
+Console.WriteLine($"\nEl total de la Billetera 1 es : ${total}\n");
+
+B2.BilletesDe10 = 7;
+B2.BilletesDe20 = 6;
+B2.BilletesDe50 = 5;
+B2.BilletesDe100 = 4;
+B2.BilletesDe200 = 3;
+B2.BilletesDe500 = 2;
+B2.BilletesDe1000 = 1;
+
+total = B2.Total();
+Console.WriteLine($"\nEl total de la Billetera 2 es : ${total}\n");
+
+
+Console.WriteLine("Billeteras combinadas: \n\n");
+
+
+Billetera billeteracombinar = B1.Combinar(B2);
+Console.WriteLine($"Billetera1: {B1.Total()}");
+Console.WriteLine($"Billetera2: {B2.Total()}");
+Console.WriteLine($"Billetera combinada: {billeteracombinar.Total()}");
+
+Console.WriteLine($"\n\nLos billetes de la billetera combinada son:\n");
+billeteracombinar.MostrarBilletes();
+
+
+
+
